6fab17e446601d5473dd5a7ad8fd8d18d9959353
gigabite-hub <king46381@gmail.com>
2022-11-15 13:21:11 +0500

    Add active class

--- main.js
+++ main.js
@@ -3,19 +3,22 @@
     $(document).ready(function() {
         
         console.log('hello');
-        
-        // 
-        var url = "http://env-apis-testing-k8s.cluviplatform.click:8082/campaigns/suppliers_near?longitude=-73.1116571&latitude=7.1004256&distance=1000&only_opened=false&limit=100";
 
-        $.ajax({
-            url: url,
-            type: 'GET',
-            datatype: 'json',
-        })
-        .done(function (result) {
-            console.log(result);
-        });
-				
+        $('.tabs-items ul li:first').addClass('active');
+        $('.tabs-items ul li:not(:first)').addClass('inactive');
+
+        $( ".tabs-items ul li" ).click(function( ) {
+
+            $('.tabs-items ul li').removeClass('active'); 
+
+            if($(this).hasClass('inactive')){ //this is the start of our condition 
+                
+                $('.tabs-items ul li').addClass('inactive');                      
+                $(this).removeClass('inactive');
+                $(this).addClass('active');
+            }
+
+		});	
 
     });
 
