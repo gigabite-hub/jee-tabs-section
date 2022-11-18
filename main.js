f0c1c833116c5798b459344688a36877414012f4
gigabite-hub <king46381@gmail.com>
2022-11-18 14:24:11 +0500

    Tabel completed

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 (function ($) {
-  
-    $(document).ready(function() {
-        
+
+    $(document).ready(function () {
+
         console.log('hello');
 
         $('.tabs-items ul li:first').addClass('active');
         $('.tabs-items ul li:not(:first)').addClass('inactive');
 
-        $( ".tabs-items ul li" ).click(function( ) {
+        $(".tabs-items ul li").click(function () {
 
-            $('.tabs-items ul li').removeClass('active'); 
+            $('.tabs-items ul li').removeClass('active');
 
-            if($(this).hasClass('inactive')){
-                
-                $('.tabs-items ul li').addClass('inactive');                      
+            if ($(this).hasClass('inactive')) {
+
+                $('.tabs-items ul li').addClass('inactive');
                 $(this).removeClass('inactive');
                 $(this).addClass('active');
             }
 
-		});	
+        });
+        
+        // Table 
+        $(".pricing-btn button").click(function () {
+            $(".pricing-container table").toggleClass("table-hide");
+        });
+
+        
 
     });
 
